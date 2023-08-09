269284c5838b605632193f2b6f002864fa807c62
Alexey Kotlyarov <a@koterpillar.com>
2023-08-09 21:00:30 +1000

    Formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,19 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  mode: 'production',
-  target: 'node18',
+  mode: "production",
+  target: "node18",
   entry: {
-    index: './src/index.ts'
+    index: "./src/index.ts",
   },
   module: {
-    rules: [
-      { test: /\.ts$/, use: 'ts-loader' }
-    ]
+    rules: [{ test: /\.ts$/, use: "ts-loader" }],
   },
   resolve: {
-    modules: [
-      require('path').resolve(__dirname, 'src')
-    ],
-    extensions: ['.ts']
+    modules: [require("path").resolve(__dirname, "src")],
+    extensions: [".ts"],
   },
   optimization: {
-    usedExports: true
-  }
+    usedExports: true,
+  },
 };
-
